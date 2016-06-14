7c6c26f7ab897a5f4749032ac60f9d0e11cf6e05
Eva <Bresciano>
2016-06-13 21:13:15 -0600

    one section left

--- Timeline/Timeline/PostDetailTableViewController.swift
+++ Timeline/Timeline/PostDetailTableViewController.swift
@@ -13,87 +13,76 @@ class PostDetailTableViewController: UITableViewController {
     
     var post: Post?
     
+    var fetchedResultsController: NSFetchedResultsController?
+    
     @IBOutlet weak var imageView: UIImageView!
     
-override func viewDidLoad() {
-    super.viewDidLoad()
-    
-    self.tableView.rowHeight = UITableViewAutomaticDimension
-
-    self.tableView.estimatedRowHeight = 60
-   }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.tableView.rowHeight = UITableViewAutomaticDimension
+        
+        self.tableView.estimatedRowHeight = 60
+    }
     
     func updateWithPost(post: Post) {
         
         imageView.image = post.photo
     }
-
-// MARK: - Table view data source
-
-override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
-    // #warning Incomplete implementation, return the number of sections
-    return 0
+    
+    func presentCommentAlert() {
+        let alertController = UIAlertController(title: "Add Comment", message: nil, preferredStyle: .Alert)
+    
+        alertController.addTextFieldWithConfigurationHandler {
+        }
+        let addCommentAction = UIAlertAction(title: "Add Comment", style: .Default) { (action) in
+            
+            guard let commentText = alertController.textFields?.first?.text,
+                let post = self.post else { return }
+            
+            PostController.sharedController.addCommentToPost(commentText, post: post, completion:nil)
+        }
+        alertController.addAction(addCommentAction)
+        
+        let cancelAction = UIAlertAction(title: "Cancel", style: .Cancel, handler: nil)
+        alertController.addAction(cancelAction)
+        
+        presentViewController(alertController, animated: true, completion: nil)
+    }
+    
+        // MARK: - Actions
+        
+        @IBAction func commentButton(sender: AnyObject) {
+            
+            presentCommentAlert()
+        }
+        
+        @IBAction func shareButton(sender: AnyObject) {
+        }
+        
+        @IBAction func followButton(sender: AnyObject) {
+        }
+        // MARK: - Table view data source
+        
+        override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+            guard let sections = fetchedResultsController?.sections else { return 1 }
+            return sections.count
+            
+        }
+        
+        override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+            guard let sections = fetchedResultsController?.sections else { return 0 }
+            let sectionInfo = sections[section]
+            return sectionInfo.numberOfObjects
+        }
+        
+        override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+            let cell = tableView.dequeueReusableCellWithIdentifier("postDetailCell", forIndexPath: indexPath)
+            if let comment = fetchedResultsController?.objectAtIndexPath(indexPath) as? Comment {
+                
+                cell.textLabel?.text = comment.text
+                return cell
+            }
+        }
+        
 }
-
-override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-    // #warning Incomplete implementation, return the number of rows
-    return 0
-}
-
-/*
- override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
- let cell = tableView.dequeueReusableCellWithIdentifier("reuseIdentifier", forIndexPath: indexPath)
- 
- // Configure the cell...
- 
- return cell
- }
- */
-
-/*
- // Override to support conditional editing of the table view.
- override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
- // Return false if you do not want the specified item to be editable.
- return true
- }
- */
-
-/*
- // Override to support editing the table view.
- override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
- if editingStyle == .Delete {
- // Delete the row from the data source
- tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
- } else if editingStyle == .Insert {
- // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
- }
- }
- */
-
-/*
- // Override to support rearranging the table view.
- override func tableView(tableView: UITableView, moveRowAtIndexPath fromIndexPath: NSIndexPath, toIndexPath: NSIndexPath) {
- 
- }
- */
-
-/*
- // Override to support conditional rearranging of the table view.
- override func tableView(tableView: UITableView, canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool {
- // Return false if you do not want the item to be re-orderable.
- return true
- }
- */
-
-/*
- // MARK: - Navigation
- 
- // In a storyboard-based application, you will often want to do a little preparation before navigation
- override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
- // Get the new view controller using segue.destinationViewController.
- // Pass the selected object to the new view controller.
- }
- */
-
-}
-
